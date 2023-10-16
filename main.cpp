1098c0943ca538b7e21bf23348720be963356748
krisi <krisi.qyra@cons.kineton.it>
2023-10-16 09:44:09 +0200

    the first commit in the polymorphism project was made just now

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+class arbin
+{
+private:
+    string emri;
+    string mbiemri;
+    int mosha;
+public:
+    arbin()
+    {
+        this->emri = "armando";
+        this->mbiemri = "preci";
+        this->mosha = 23;
+    }
+    arbin(string emri,string mbiemri,int mosha)
+    {
+        this->emri = emri;
+        this->mbiemri = mbiemri;
+        this->mosha = mosha;
+
+    }
+    string print_details();
+
+};
+
+string arbin::print_details()
+{
+    return "Emri: " + this->emri + "\nMbiemri: " + this->mbiemri + "\nMosha: " + to_string(this->mosha);
+}
+
+int main()
+{
+    arbin arbin2("Arbin","Bici",22);
+    arbin armando;
+    cout << arbin2.print_details()<<endl;
+    cout<< "\r\n"<<endl;
+    cout << armando.print_details()<<endl;
+
+
+    return 0;
+}
